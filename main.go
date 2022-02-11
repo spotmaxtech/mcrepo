96f3bde137c7d6fea77f99744b95394ff661c8c7
Liuzoxan <liuzoxan@gmail.com>
2022-02-11 22:02:32 +0800

    update

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
+	"github.com/spotmaxtech/mcrepo/pkg/cmd"
 )
 
 func main() {
@@ -11,7 +12,11 @@ func main() {
 	rootCmd := &cobra.Command{
 		Use:   "mcrepo",
 		Short: "maxcloud multi repo management tool",
+		Long:  "maxcloud multi repo management tool",
 	}
+
+	cmd.RegistryCmd.AddCommand(cmd.RegistryList)
+	rootCmd.AddCommand(cmd.RegistryCmd)
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
 	}
--- pkg/cmd/registry.go
+++ pkg/cmd/registry.go
@@ -1,1 +1,24 @@
 package cmd
+
+import (
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+)
+
+var RegistryCmd = &cobra.Command{
+	Use:   "registry",
+	Short: "registry management",
+	Long:  "registry management",
+	//Run: func(cmd *cobra.Command, args []string) {
+	//	logrus.SetLevel(logrus.DebugLevel)
+	//},
+}
+
+var RegistryList = &cobra.Command{
+	Use:   "list",
+	Short: "registry list",
+	Long:  "registry list",
+	Run: func(cmd *cobra.Command, args []string) {
+		logrus.Infof("registry list command run")
+	},
+}
