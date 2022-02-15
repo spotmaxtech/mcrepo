b543c7f5eafe6712dd9fe29435c816b037d0ed1a
Liuzoxan <liuzoxan@gmail.com>
2022-02-15 15:19:40 +0800

    update

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
+	"github.com/spotmaxtech/mcrepo/pkg"
 	"github.com/spotmaxtech/mcrepo/pkg/cmd"
 )
 
@@ -18,6 +19,10 @@ func main() {
 	rootCmd.AddCommand(cmd.RegistryCmd)
 	rootCmd.AddCommand(cmd.RepoCmd)
 	rootCmd.AddCommand(cmd.ImageCmd)
+
+	pkg.InitConfig()
+	pkg.InitAliyunRegistryMap(pkg.GMcrepoConfig.Aliyun)
+
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
 	}
--- pkg/cmd/registry.go
+++ pkg/cmd/registry.go
@@ -1,8 +1,11 @@
 package cmd
 
 import (
+	"fmt"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
+	"github.com/spotmaxtech/gokit"
+	"github.com/spotmaxtech/mcrepo/pkg"
 )
 
 var RegistryCmd = &cobra.Command{
@@ -20,6 +23,16 @@ var RegistryList = &cobra.Command{
 	},
 }
 
+var RegistryAll = &cobra.Command{
+	Use:   "all",
+	Short: "registry all",
+	Long:  "registry all",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println(gokit.PrettifyYaml(pkg.GMcrepoConfig))
+	},
+}
+
 func init() {
+	RegistryCmd.AddCommand(RegistryAll)
 	RegistryCmd.AddCommand(RegistryList)
 }
